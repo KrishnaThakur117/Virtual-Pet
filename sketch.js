3810f3307322d3d82deb2d4dd7db9ec81caabe46
Krishna Thakur <71776030+KrishnaThakur117@users.noreply.github.com>
2020-10-01 15:08:10 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+var dog,happydog,foodS,foodStock,database;
+function preload()
+{
+  //load images here
+  this.image = loadImage("images/dogImg.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog = createSprite(250,250,10,50);
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
